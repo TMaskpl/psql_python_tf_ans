efbe1b3dd5491fa24eca165c0f31cb29e15fc607
Daniel Niemczok <daniel@niemczok.pl>
2023-05-31 09:55:46 +0200

    get_json_data OK

--- get_date_psql.py
+++ get_date_psql.py
@@ -4,7 +4,7 @@ from env import *
 
 import psycopg2
 import json
-import re
+import os
 
 
 def get_connection():
@@ -19,7 +19,7 @@ def get_connection():
     except:
         return False
 
-def get_select(conn):
+def get_customer(conn):
     curr = conn.cursor()
     curr.execute("SELECT id, nazwa FROM public.customer;")
     data = curr.fetchall()
@@ -27,27 +27,29 @@ def get_select(conn):
     list = []
     
     for row in data:
-        id = str(row[0]).replace('(','').replace(')','').replace("'","").replace(',','')
-        nazwa = str(row[1]).replace('(','').replace(')','').replace("'","").replace(',','')
+        id = row[0]
+        nazwa = row[1]
         
         list.append({"id": id})
         list.append({"nazwa": nazwa})
     
-    write_to_json('output.json')
+    write_to_json(output_file, list)
 
     conn.close()
     
-def write_to_json(output_file):
+def write_to_json(output_file, list):
+    if os.path.exists(output_file):
+        os.remove(output_file)
+        
     with open(output_file, 'w', encoding='utf-8') as f:
-        json.dump(dict, f, ensure_ascii=False, indent=4)
+        json.dump(list, f, ensure_ascii=False, indent=4)
 
 def get_json_data(output_file):
     with open(output_file) as json_data:
         data = json.load(json_data)
-
     print(data[1]["nazwa"])
 
 if __name__=='__main__':
-    # conn = get_connection()
-    # get_select(conn)
+    conn = get_connection()
+    get_customer(conn)
     get_json_data(output_file)
