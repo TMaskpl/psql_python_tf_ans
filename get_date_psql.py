54e87894b4cf1d19eeb29c635609c759fc2051bd
Daniel Niemczok <daniel@niemczok.pl>
2023-05-31 13:15:43 +0200

    get_customer_vars(conn, customer)

--- get_date_psql.py
+++ get_date_psql.py
@@ -6,7 +6,7 @@ import psycopg2
 import json
 import os
 
-
+### CONN TO PSQL
 def get_connection():
     try:
         return psycopg2.connect(
@@ -21,7 +21,7 @@ def get_connection():
 
 def get_customer(conn):
     curr = conn.cursor()
-    curr.execute("SELECT id, nazwa FROM public.customer;")
+    curr.execute("SELECT id, nazwa FROM public.customer WHERE nazwa = 'TMaskPL';")
     data = curr.fetchall()
 
     list = []
@@ -36,7 +36,9 @@ def get_customer(conn):
     write_to_json(output_file, list)
 
     conn.close()
-    
+
+
+### JSON
 def write_to_json(output_file, list):
     if os.path.exists(output_file):
         os.remove(output_file)
@@ -49,7 +51,36 @@ def get_json_data(output_file):
         data = json.load(json_data)
     print(data[1]["nazwa"])
 
+
+### VARS
+def get_customer_vars(conn, customer):
+    curr = conn.cursor()
+    curr.execute(f"SELECT id, nazwa FROM public.customer WHERE nazwa = {customer};")
+    data = curr.fetchall()
+    
+    if os.path.exists(output_file_vars):
+        os.remove(output_file_vars)
+
+    list = []
+    
+    for row in data:
+        id = row[0]
+        nazwa = row[1]
+        
+        list.append(f"id: '{id}'\n")
+        list.append(f"nazwa: '{nazwa}'\n")
+
+    for l in list:
+        with open(output_file_vars, 'a', encoding='utf-8') as f:
+            f.write(l)
+    
+    conn.close()
+
+
+
+### MAIN
 if __name__=='__main__':
     conn = get_connection()
-    get_customer(conn)
-    get_json_data(output_file)
+    # get_customer(conn)
+    # get_json_data(output_file)
+    get_customer_vars(conn, customer)
