43e14b8a372143ab5baf5970cf9f8eac15441f0e
Daniel Niemczok <daniel@niemczok.pl>
2023-05-31 08:50:39 +0200

    get_json_data fix

--- get_date_psql.py
+++ get_date_psql.py
@@ -5,7 +5,7 @@ import psycopg2
 import json
 import re
 
- 
+
 def get_connection():
     try:
         return psycopg2.connect(
@@ -17,34 +17,32 @@ def get_connection():
         )
     except:
         return False
- 
 
 def get_select(conn):
     curr = conn.cursor()
     curr.execute("SELECT id, nazwa FROM public.customer;")
     data = curr.fetchall()
 
-    dict = {}
-    parameters = []
+    list = []
     
     for row in data:
         id = str(row[0]).replace('(','').replace(')','').replace("'","").replace(',','')
         nazwa = str(row[1]).replace('(','').replace(')','').replace("'","").replace(',','')
         
-        dict.update({"id": id})
-        dict.update({"nazwa": nazwa})
-        parameters.append(dict)
+        list.append({"id": id})
+        list.append({"nazwa": nazwa})
 
     with open('output.json', 'w', encoding='utf-8') as f:
-        json.dump(parameters, f, ensure_ascii=False, indent=4)
+        json.dump(dict, f, ensure_ascii=False, indent=4)
 
     conn.close()
     
-
-def get_jason_data():
-    pass
+def get_json_data(file):
+    with open(file) as json_data:
+        data = json.load(json_data)
+    print(data[0])
 
 if __name__=='__main__':
-    # conn = get_connection()
-    # get_select(conn)
-    get_jason_data()
+    conn = get_connection()
+    get_select(conn)
+    # get_json_data("output.json")
